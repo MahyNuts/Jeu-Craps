9da4353d2ef35ce7d0551516accde062b8373245
Mahy Clément <210305@CCM-Asty-Moulin.local>
2023-03-10 16:15:12 +0100

    .

---
+++ CodeCraps/Craps/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Craps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tokens = 10;
+            int mise;
+            int de1;
+            int de2;
+            int sommeDes;
+            int saveSomme;
+            @base bases = new @base();
+            Console.WriteLine("Vous commencez avec " + tokens + " jetons.");
+            Console.WriteLine("");
+            bases.miseJoueur(tokens, out mise);
+            Console.WriteLine("");
+            bases.lanceDes(out de1, out de2, out sommeDes);
+            Console.WriteLine("Les dés tirés sont : "+de1+" et "+de2+".");
+            Console.WriteLine("La somme des dés est de " + sommeDes + ".");
+            if(sommeDes==7 || sommeDes == 11)
+            {
+                tokens = tokens + mise;
+            }
+            else
+            {
+                if (sommeDes == 2 || sommeDes == 3 || sommeDes == 12)
+                {
+                    tokens = tokens - mise;
+                }
+                else
+                {
+                    saveSomme = sommeDes;
+                    while(sommeDes != saveSomme || sommeDes == 7 || sommeDes == 11 || sommeDes == 2 || sommeDes == 3 || sommeDes == 12)
+                    {
+                        bases.lanceDes(out de1, out de2, out sommeDes);
+                    }
+                    if(sommeDes == saveSomme)
+                    {
+                        Console.WriteLine("Vous êtes tombé sur la somme exacte. Vous remportez votre mise."); 
+                    }
+                    else
+                    {
+                        if (sommeDes == 7 || sommeDes == 11)
+                        {
+                            Console.WriteLine("Vous êtes tombé sur " + sommeDes + ". Vous remportez votre mise.");
+                        }
+                        else (sommeDes == 2 || sommeDes == 3 || sommeDes == 12)
+                        {
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ CodeCraps/Craps/base.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Craps
+{
+    public struct @base
+    {
+        public void miseJoueur(int tokens, out int mise)
+        {
+            do
+            {
+                Console.WriteLine("Combien voulez-vous misez ? ");
+                int.TryParse(Console.ReadLine(), out mise);
+                if (mise == 0)
+                {
+                    Console.WriteLine("Impossible de ne rien miser.");
+                }
+                else
+                {
+                    if (mise < 0)
+                    {
+                        Console.WriteLine("Pas la peine d'essayer.");
+                    }
+                    else
+                    {
+                        if (mise > 10)
+                        {
+                            Console.WriteLine("Vous n'avez pas assez de jetons");
+                        }
+                    }
+                }
+                Console.WriteLine("");
+            } while (mise > tokens || mise < 1);
+
+            Console.WriteLine("Vous misez " + mise + " jetons.");
+        }
+
+        public void lanceDes(out int de1, out int de2, out int sommeDes)
+        {
+            Random rand = new Random();
+            de1 = rand.Next(1, 6);
+            de2 = rand.Next(1, 6);
+            sommeDes = de1 + de2;
+        }
+    }
+}
